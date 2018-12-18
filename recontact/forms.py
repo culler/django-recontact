21fe36cea92e954101b3c2f3b1c9a7997abdc008
Marc Culler <Marc Culler@localhost>
2018-12-17 20:59:32 -0600

    Do not validate messages with URLs.  They are not necessary in a contact message and just invite the many click-bait spammers who have hacked Google recaptcha.

--- recontact/forms.py
+++ recontact/forms.py
@@ -1,18 +1,29 @@
 from django.forms import Form, CharField, EmailField, Textarea, TextInput, EmailInput
+from django.core.exceptions import ValidationError
+
+import re
+
+has_url = re.compile("https?://") 
+
+def no_click_bait(value):
+    if has_url.search(value):
+        raise ValidationError('Please remove all URLs.')
 
 class RecontactForm(Form):
     """
     Django form which collects data for a contact email message.
     """
     sender = CharField(label='', widget=TextInput(
-        attrs={'placeholder': 'Your Name',
-               'class': 'recontact'}))
-    reply_to=EmailField(label='', widget=EmailInput(
-        attrs={'placeholder': 'Your email address',
-               'class': 'recontact'}))
+        attrs={'placeholder': 'Your Name', 'class': 'recontact'}),
+        validators=[no_click_bait])
+    
+    reply_to = EmailField(label='', widget=EmailInput(
+        attrs={'placeholder': 'Your email address', 'class': 'recontact'}))
+
     subject = CharField(label='', widget=TextInput(
-        attrs={'placeholder': 'Subject',
-               'class': 'recontact'}))
+        attrs={'placeholder': 'Subject', 'class': 'recontact'}),
+        validators=[no_click_bait])
+
     message = CharField(label='', widget=Textarea(
-        attrs={'placeholder': 'Message',
-               'class': 'recontact'}))
+        attrs={'placeholder': 'Message', 'class': 'recontact'}),
+        validators=[no_click_bait])
